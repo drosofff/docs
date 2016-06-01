4337f9a0d630b68f73b9cb040c2f89009e39386f
drosofff <drosofff@gmail.com>
2016-06-02 00:47:23 +0200

    revert to marius method

--- deploy.sh
+++ deploy.sh
@@ -2,35 +2,43 @@
 
 # We start by building and deploying the documentation
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
-sudo pip install mkdocs
-mkdir mkdocs_build
-cd mkdocs_build
-# Initialize gh-pages checkout
-# mkdir -p docs/html
-
-#(
-#    cd docs/html
-#    git init
-#    git config user.name "${GH_USER_NAME}"
-#    git config user.email "${GH_USER_EMAIL}"
-#    git remote add upstream "https://${GH_TOKEN}@${GH_REF}"
-#    git fetch upstream
-#    git reset upstream/gh-pages
-#)
 DATE=`date`
-git clone https://github.com/drosofff/docs.git
-cd docs
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
+sudo pip install mkdocs
+# Initialize gh-pages checkout
+mkdir -p docs/html
+(
+cd docs/html
+git init
+git config user.name "${GH_USER_NAME}"
+git config user.email "${GH_USER_EMAIL}"
+git remote add upstream "https://$GH_TOKEN@${GH_REF}"
+git fetch upstream
+git reset upstream/gh-pages
+)
 
+cd $SCRIPT_PATH
+mkdocs build --clean --site-dir docs/html
 
 # Commit and push the documentation to gh-pages
-#(
-#    cd docs/html
-#    touch .
-#    git add -A .
-#    git commit -m "Rebuild pages at ${rev}"
-#    git push -q upstream HEAD:gh-pages
-#)
+(
+cd docs/html
+touch .
+git add -A .
+git commit -m "Rebuild pages at ${rev} $DATE"
+git push -q upstream HEAD:gh-pages
+)
 cd $SCRIPT_PATH
+
+# alternate that fails because mkdocs gh-deploy does not takes the credentials in .git/credentials
+#SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+#sudo pip install mkdocs
+#mkdir mkdocs_build
+#cd mkdocs_build
+# Initialize gh-pages checkout
+# mkdir -p docs/html
+#DATE=`date`
+#git clone https://github.com/drosofff/docs.git
+#cd docs
+#git config credential.helper "store --file=.git/credentials"
+#echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+#mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
